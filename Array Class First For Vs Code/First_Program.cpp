7d20b94c7ea3114334c376bd531862c963518a3e
Sonu Kumar Singh <sonusinghsengar28@gmail.com>
2024-12-21 17:32:35 +0530

    Add files via upload

---
+++ Array Class First For Vs Code/First_Program.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // array declaration 
+
+    int arr[4];
+    // int arr[]= {2,3,4,5,6,7,8};
+
+
+    int count[23]; 
+
+ // in this declaration in the top of as first int is a datatype and 
+ // count is a varable name   and 
+ // arr[23] is a  size of array  ; this is a terminator
+
+   cout<< arr << endl;
+   cout<<" Array Declaration successfully :" << &arr << endl;
+
+   
+
+
+return 0;
+}
